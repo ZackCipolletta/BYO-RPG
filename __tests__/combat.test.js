0fd3cade19abd31fec43efd2ded89926987f4a41
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-02-02 08:22:28 -0800

    test new function to check if combat should continue and if so, to call a function that will call the next monster.
    Co-authored-by: Zookerman3 <szook7@gmail.com>

--- __tests__/combat.test.js
+++ __tests__/combat.test.js
@@ -1,64 +1,74 @@
 import { Character, chooseCharacter } from "../src/js/character.js";
 import { Monster, createZombie } from "../src/js/monster.js";
-import { heroVsMonster, levelUp, nextMonsterFunc } from "../src/js/combat";
+import { heroVsMonster, levelUp, continueCombatFunc, nextMonsterFunc } from "../src/js/combat";
 
 describe('heroVsMonster', () => {
-    test("should grab the warrior.hp, warrior.ap and zombie.hp, zombie.ap", () => {
-        let hero = chooseCharacter(1);
-        let zombie = createZombie();
-        expect(hero.hp).toEqual(10);
-        expect(hero.ap).toEqual(10);
-        expect(zombie.hp).toEqual(15);
-        expect(zombie.ap).toEqual(5);
-    });
+  test("should grab the warrior.hp, warrior.ap and zombie.hp, zombie.ap", () => {
+    let hero = chooseCharacter(1);
+    let zombie = createZombie();
+    expect(hero.hp).toEqual(10);
+    expect(hero.ap).toEqual(10);
+    expect(zombie.hp).toEqual(15);
+    expect(zombie.ap).toEqual(5);
+  });
 
 
-    test("should find the difference of hero.hp and zombie.ap and the difference between zombie.hp and hero.ap", () => {
-        let hero = chooseCharacter(1);
-        let zombie = createZombie();
-        heroVsMonster(hero, zombie);
-        expect(hero.hp).toEqual(5);
-        expect(zombie.ap).toEqual(5);
-        expect(zombie.hp).toEqual(5);
-        expect(hero.ap).toEqual(10);
-    });
+  test("should find the difference of hero.hp and zombie.ap and the difference between zombie.hp and hero.ap", () => {
+    let hero = chooseCharacter(1);
+    let zombie = createZombie();
+    heroVsMonster(hero, zombie);
+    expect(hero.hp).toEqual(5);
+    expect(zombie.ap).toEqual(5);
+    expect(zombie.hp).toEqual(5);
+    expect(hero.ap).toEqual(10);
+  });
 
-    test("should check if the current monster is dead by hp <= 0", () => {
-        let hero = chooseCharacter(1);
-        hero.ap = 15;
-        let zombie = createZombie();
-        heroVsMonster(hero, zombie);
-        expect(zombie.hp).toBeLessThanOrEqual(0);
-    });
+  test("should check if the current monster is dead by hp <= 0", () => {
+    let hero = chooseCharacter(1);
+    hero.ap = 15;
+    let zombie = createZombie();
+    heroVsMonster(hero, zombie);
+    expect(zombie.hp).toBeLessThanOrEqual(0);
+  });
 });
 
 describe("levelUp", () => {
-    test("should take the current level and add 1", () => {
-        let hero = chooseCharacter(1);
-        levelUp(hero);
-        expect(hero.level).toEqual(2);
-    });
+  test("should take the current level and add 1", () => {
+    let hero = chooseCharacter(1);
+    levelUp(hero);
+    expect(hero.level).toEqual(2);
+  });
 
-    test("should check what role the hero is and change the total hp based on chosen role.", () => {
-        let hero = chooseCharacter(1);
-        levelUp(hero);
-        expect(hero.hp).toEqual(15);
-    });
+  test("should check what role the hero is and change the total hp based on chosen role.", () => {
+    let hero = chooseCharacter(1);
+    levelUp(hero);
+    expect(hero.hp).toEqual(15);
+  });
 
-    test("should check what role the hero is and change the total ap based on chosen role.", () => {
-        let hero = chooseCharacter(2);
-        levelUp(hero);
-        expect(hero.ap).toEqual(18);
-    });
+  test("should check what role the hero is and change the total ap based on chosen role.", () => {
+    let hero = chooseCharacter(2);
+    levelUp(hero);
+    expect(hero.ap).toEqual(18);
+  });
 });
 
+describe("continueCombatFunc", () => {
+  test("should check if the response is y or n, then if the response is y, call the nextMonsterFunc", () => {
+    let hero = chooseCharacter(2);
+    levelUp(hero);
+    let response = 'y';
+    continueCombatFunc(hero, response);
+    expect(continueCombatFunc).toEqual('call nextMonsterFunc');
+  })
+})
+
 // describe("nextMonsterFunc", () => {
-//     test("should check the current level of the hero and call a monster based on the hero's current level", () => {
-//         let hero = chooseCharacter(1);
-//         hero.level = 2;
-//         nextMonsterFunc(hero);
-//         expect(nextMonsterFunc()).toEqual("OwlBear");
-//     });
+//   test("should check the current level of the hero and call a monster based on the hero's current level", () => {
+//     let hero = chooseCharacter(1);
+//     hero.level = 2;
+//     nextMonsterFunc(hero);
+//     expect(nextMonsterFunc()).toEqual("OwlBear");
+//   });
 // });
 // describe('combatFunc'), () => {
 //     test("should return updated hp values after the function runs")
--- src/js/combat.js
+++ src/js/combat.js
@@ -2,35 +2,41 @@ import { Character, chooseCharacter } from "./character.js";
 import { Monster, createZombie, createOwlBear, createCthulu } from "./monster.js";
 
 export function heroVsMonster(heroCall, monsterCall) {
-    let hero = heroCall;
-    let zombie = monsterCall;
-    hero.hp = hero.hp - zombie.ap;
-    zombie.hp = zombie.hp - hero.ap;
-    if (zombie.hp <= 0) {
-        levelUp(hero);
-        nextMonsterFunc(hero);
-    }
+  let hero = heroCall;
+  let zombie = monsterCall;
+  hero.hp = hero.hp - zombie.ap;
+  zombie.hp = zombie.hp - hero.ap;
+  if (zombie.hp <= 0) {
+    levelUp(hero);
+    continueCombatFunc(hero);
+  }
 }
 
 export function levelUp(currentHero) {
-    currentHero.level++;
-    currentHero.hp = currentHero.hpCapacity + 5;
-    currentHero.hpCapacity = currentHero.hp;
-    currentHero.ap = currentHero.ap + 5;
-    return currentHero;
+  currentHero.level++;
+  currentHero.hp = currentHero.hpCapacity + 5;
+  currentHero.hpCapacity = currentHero.hp;
+  currentHero.ap = currentHero.ap + 5;
+  return currentHero;
 }
 
-export function nextMonsterFunc(currentHero) {
-    let currentMonster;
-    if (currentHero.level === 1) {
-        currentMonster = createZombie();
-    } else if (currentHero.level === 2) {
-        createOwlBear();
-    } else if (currentHero.level === 3) {
-        currentMonster = createCthulu();
-    }
-    heroVsMonster(currentHero, currentMonster);
+export function continueCombatFunc(hero, response) {
+  // if (response === 'y') {
+  //   nextMonsterFunc(hero)
+  // }
 }
+
+// export function nextMonsterFunc(currentHero) {
+//   let currentMonster;
+//   if (currentHero.level === 1) {
+//     currentMonster = createZombie();
+//   } else if (currentHero.level === 2) {
+//     createOwlBear();
+//   } else if (currentHero.level === 3) {
+//     currentMonster = createCthulu();
+//   }
+//   heroVsMonster(currentHero, currentMonster);
+// }
 
 // function checkHp(hero, monster) {
 //     console.log('hero stats:' + hero.hp + ". Monster stats: " + monster.hp );
